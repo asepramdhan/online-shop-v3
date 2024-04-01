1f5fbe355e11534edb823706a93c09b82446ac2c
asepramdhan <asiepramdhan@gmail.com>
2024-04-02 00:20:42 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        // You will probably also need these lines
+        "./resources/**/**/*.blade.php",
+        "./resources/**/**/*.js",
+        "./app/View/Components/**/**/*.php",
+        "./app/Livewire/**/**/*.php",
+
+        // Add mary
+        "./vendor/robsontenorio/mary/src/View/Components/**/*.php",
+    ],
+    theme: {
+        container: {
+            padding: {
+                DEFAULT: "1rem",
+                sm: "2rem",
+                lg: "4rem",
+                xl: "5rem",
+                "2xl": "6rem",
+            },
+        },
+        extend: {},
+    },
+    // Add daisyUI
+    plugins: [require("daisyui")],
+};
