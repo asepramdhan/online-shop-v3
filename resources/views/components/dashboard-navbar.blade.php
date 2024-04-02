4191636baa1bfc70f4dc34331d7fbdfbd1836be6
asepramdhan <asiepramdhan@gmail.com>
2024-04-02 23:20:23 +0700

    delete messages dan akan di ganti ke whatsapp

--- resources/views/components/dashboard-navbar.blade.php
+++ resources/views/components/dashboard-navbar.blade.php
@@ -22,8 +22,6 @@
 
   <x-slot:actions>
 
-    <x-button label="Messages" icon="o-envelope" link="/auth/messages" class="btn-ghost btn-sm" responsive />
-
     <x-button label="Notifications" icon="o-bell" link="/auth/notifications" class="btn-ghost btn-sm" responsive />
 
   </x-slot:actions>
